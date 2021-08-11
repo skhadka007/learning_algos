c8ece9c8d942ba6cefe99d9a079bd9b8ff85fc09
Santosh Khadka <skhadka.code@gmail.com>
2021-08-11 14:01:18 -0500

    FeedbackWebApp various updates

--- ASP.NET/FeedbackWebApp/Data/ApplicationDbContext.cs
+++ ASP.NET/FeedbackWebApp/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FeedbackWebApp.Models;
 
 namespace FeedbackWebApp.Data
 {
@@ -12,5 +13,6 @@ namespace FeedbackWebApp.Data
             : base(options)
         {
         }
+        public DbSet<FeedbackWebApp.Models.Feedback> Feedback { get; set; }
     }
 }
