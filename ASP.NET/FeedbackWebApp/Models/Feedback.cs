9e7ddcb3c2740697680578567dedf3f9a805c438
Santosh Khadka <skhadka.code@gmail.com>
2021-08-11 12:37:54 -0500

    Feedback WebApp

---
+++ ASP.NET/FeedbackWebApp/Models/Feedback.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedbackWebApp.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        public string FeedbackQ { get; set; }
+
+        public string FeedbackA { get; set; }
+
+        public Feedback()
+        {
+
+        }
+
+    }
+}
