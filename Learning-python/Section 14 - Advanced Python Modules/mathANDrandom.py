87ac8913180e8ef9ce9b03a364c98763393aba4a
Santosh Khadka <skhadka.code@gmail.com>
2021-09-21 20:24:26 -0500

    python math

---
+++ Learning-python/Section 14 - Advanced Python Modules/mathANDrandom.py
@@ -0,0 +1,31 @@
+# Santosh Khadka
+import math
+
+# help(math)    # Shows help data. What it supports etc.
+
+value = 4.55
+
+# print(math.floor(value))        # always removes the decimal place. Always rounds down.
+# print(math.ceil(value))         # always rounds up. Opposite of floor()
+# print(round(4.5))               # Output: 4: X.5 will always round down when even 
+# print(round(5.5))               # Output: 6: X.5 will always round up when odd     - Done to get ride of a large ammount of over rounding to just top/bottom. 
+
+# print(math.pi)  # 3.141592653589793
+# from math import pi     # dont need to use 'math'.pi
+# print(pi)       # 3.141592653589793
+
+# print(math.e)
+# print(math.inf)     # infiniti      "inf"
+# print(math.nan)     # Not a Number  "nan"
+
+'''
+Numpy library used for heavy math usuage. Ifficient and deeper than just the math module
+'''
+
+# print(math.log(math.e))
+# print(math.log(100, 10))        # value, base. So what number(x) is: 100 = 10^x     x = 2 -> 10^2 = 100
+
+# print(math.sin(10))         # output is in radians
+# print(math.degrees(math.pi/2))
+# print(math.radians(180))
+
